a51202adb8b3e6447636c6a4051253d7284dbddb
PRAVIN <pravin@PRAVINs-MacBook-Air.local>
2022-10-31 16:54:57 +0530

    bug fixed

--- app.js
+++ app.js
@@ -11,10 +11,16 @@ const cors = require('cors')
 //const bodyParser = require('body-parser')
 //const path = require('path')
 //require('dotenv/config');
+require('./models/user')
+require('./models/post')
 
-app.use(
-    cors({origin: '*'})
-)
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+}
+ 
+app.use(cors(corsOptions))
 
 require('./models/user')
 require('./models/post')
@@ -23,7 +29,7 @@ require('./models/post')
 //app.use(bodyParser.json())
 
 
-app.use(cors())
+
 app.use(express.json())
 app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
