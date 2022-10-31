f6513fda8bebfd37e49bdb090f2dcfb8a41dcc62
PRAVIN <pravin@PRAVINs-MacBook-Air.local>
2022-10-31 16:42:09 +0530

    bug fixed

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const cors = require('cors')
 //require('dotenv/config');
 
 app.use(
-    cors({origin: ['http://localhost:3000', 'http://127.0.0.1:3000']})
+    cors({origin: '*'})
 )
 
 require('./models/user')
